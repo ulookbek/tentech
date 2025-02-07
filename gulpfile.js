e1249efce68c561e8a81de78aff8e1bc6b515452
ulookbek <ulukbekkaparbekov@gmail.com>
2025-02-07 17:34:22 +0600

    save 2

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,20 @@
 const gulp = require('gulp');
 
 const fileInclude = require('gulp-file-include');
+const replace = require('gulp-replace');
+
+function fixImagePaths() {
+    return gulp.src('src/**/*.html')               // берем все HTML-файлы из папки src
+        .pipe(replace(/src=(["'])\.\.\/img\//g, 'src=$1/tentech/img/')) // заменяем пути
+        .pipe(gulp.dest('docs'));                      // сохраняем результат в папку docs
+}
+
+// Если изображения в CSS, можно аналогично заменить пути:
+function fixCssPaths() {
+    return gulp.src('src/**/*.css')
+        .pipe(replace(/url\(["']?img\//g, 'url("/tentech/img/'))
+        .pipe(gulp.dest('docs'));
+}
 
 function html() {
     return gulp
@@ -18,4 +32,4 @@ function html() {
 // Определяем экспортируемые задачи
 exports.build = html;
 // Если хотите, чтобы по умолчанию сборка запускалась и затем следила за изменениями:
-exports.default = gulp.series(html);
+exports.default = gulp.series(html, fixImagePaths, fixCssPaths);
