8a5dfb5ca0476afab0e538038d700d1bcb795ad2
ulookbek <ulukbekkaparbekov@gmail.com>
2025-02-08 17:57:47 +0600

    save 3

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,46 @@
-// gulpfile.js
-const gulp = require('gulp');
+import gulp from 'gulp';
+import fileInclude from 'gulp-file-include';
+import replace from 'gulp-replace';
 
-const fileInclude = require('gulp-file-include');
-const replace = require('gulp-replace');
+// Пути к исходным и собранным файлам
+const paths = {
+    html: {
+        src: 'src/**/*.html',
+        dest: 'docs/',
+    },
+    css: {
+        src: 'css/**/*.css',
+        dest: 'docs/css/',
+    },
+    images: {
+        src: 'img/**/*.{jpg,jpeg,png,svg,gif}',
+        dest: 'docs/img/',
+    },
+};
 
-function fixImagePaths() {
-    return gulp.src('src/**/*.html')               // берем все HTML-файлы из папки src
-        .pipe(replace(/src=(["'])\.\.\/img\//g, 'src=$1/tentech/img/')) // заменяем пути
-        .pipe(gulp.dest('docs'));                      // сохраняем результат в папку docs
-}
-
-// Если изображения в CSS, можно аналогично заменить пути:
-function fixCssPaths() {
-    return gulp.src('src/**/*.css')
-        .pipe(replace(/url\(["']?img\//g, 'url("/tentech/img/'))
-        .pipe(gulp.dest('docs'));
-}
-
-function html() {
-    return gulp
-        .src(['./src/**/*.html'])
+// Обработка HTML с инклюдами
+export const html = () =>
+    gulp
+        .src(paths.html.src)
         .pipe(
             fileInclude({
-                prefix: '@@',
-                basepath: '@file',
+                prefix: '@@', // Префикс для инклюдов
+                basepath: '@file', // Базовый путь к инклюдам
             })
         )
-        .pipe(gulp.dest('docs'));
-}
+        // Исправление путей к изображениям
+        .pipe(replace(/(\.\.\/)+img\//g, './img/')) // Заменяем все '../img/' на './img/'
+        .pipe(gulp.dest(paths.html.dest));
 
-// Определяем экспортируемые задачи
-exports.build = html;
-// Если хотите, чтобы по умолчанию сборка запускалась и затем следила за изменениями:
-exports.default = gulp.series(fixImagePaths, fixCssPaths, html);
+// Копирование CSS
+export const css = () =>
+    gulp.src(paths.css.src).pipe(gulp.dest(paths.css.dest));
+
+// Копирование изображений
+export const images = () =>
+    gulp.src(paths.images.src).pipe(gulp.dest(paths.images.dest));
+
+// Основная задача сборки
+export const build = gulp.series(html, css, images);
+
+export default build;
